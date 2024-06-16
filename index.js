5373d018b70ca693c95ffb7bcbf9beadb2f376de
RUHULAMIN2024 <ruhulamin15jun1999@gmail.com>
2024-06-16 06:59:19 +0600

    forum post detailes data get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
 
@@ -79,6 +79,14 @@ async function run() {
         .toArray();
       res.send(result);
     });
+
+    app.get("/forum-posts-detailes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await forumPostsCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
